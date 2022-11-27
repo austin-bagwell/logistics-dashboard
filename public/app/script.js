62269b6c603ce7a08b5cdd0a8a1da09bc3ad2962
austin-bagwell <austinbagwell.dev@gmail.com>
2022-11-26 20:59:45 -0500

    struggling with event listener page reloading

--- public/app/script.js
+++ public/app/script.js
@@ -1,3 +1,22 @@
 "use strict";
-
 console.log(`Hello from /public/app/script.js`);
+
+// FIXME reloads page on every function call
+const sidenavItems = document.querySelectorAll(".sidenav__item");
+
+// sidenavItems.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     console.log(`clicked on e: ${e}`);
+//     sidenavItems.forEach((f) => {
+//       f.classList.remove("active");
+//     });
+//     e.target.classList.toggle("active");
+//   });
+// });
+
+const test = document.querySelector("#test");
+
+test.addEventListener("click", () => {
+  test.classList.toggle("active");
+  test.preventDefault();
+});
