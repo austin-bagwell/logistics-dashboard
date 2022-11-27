493ce748b4b471895e14fa203ab55db4d38105b1
austin-bagwell <austinbagwell.dev@gmail.com>
2022-11-26 19:05:23 -0500

    setup express server, nodemon

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
