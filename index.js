577585a92b46ed28219f8ab3f3b0e25bd4309bc6
austin-bagwell <austinbagwell.dev@gmail.com>
2022-11-26 20:09:04 -0500

    add script.js to public - better routing tbd

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 "use strict";
+// for file structure examples:
+// https://stackoverflow.com/questions/18927298/node-js-project-naming-conventions-for-files-folders/20885809#20885809
+
 const express = require("express");
 const path = require("path");
 
@@ -6,6 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use("/", express.static(path.join(__dirname, "public")));
+// app.use("/", express.static(path.join(__dirname, "public/app")));
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
