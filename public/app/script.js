50b127342dc19a74a7c0aa61edd3e80acbd1c04d
austin-bagwell <austinbagwell.dev@gmail.com>
2022-11-29 15:53:16 -0500

    sidenav now links to new page

--- public/app/script.js
+++ public/app/script.js
@@ -1,38 +1,30 @@
 "use strict";
 console.log(`Hello from /public/app/script.js`);
 
-// FIXME reloads page on every function call
-const sidenavItems = document.querySelectorAll(".sidenav__item");
-
-// sidenavItems.forEach((btn) => {
-//   btn.addEventListener("click", (e) => {
-//     console.log(`clicked on e: ${e}`);
-//     sidenavItems.forEach((f) => {
-//       f.classList.remove("active");
-//     });
-//     e.target.classList.toggle("active");
-//   });
-// });
-
+// FIXME how to assign 'active' to specific e.target
+// the below will add 'active' to the li class list, but only if you click the li element specifically - obviously this sucks
 const btnsSidenav = document.querySelector(".sidenav__items");
 
 btnsSidenav.addEventListener("click", (e) => {
-  e.preventDefault();
-  // FIXME how to assign 'active' to specific e.target
-  const btnComponents = ["A" /*, "IMG", "LI", "P"*/];
+  //   e.preventDefault();
+
+  const btnComponents = ["A", "IMG", "LI", "P"];
 
   if (btnComponents.includes(e.target.tagName.toUpperCase())) {
-    console.log(`clicked a link button`);
-    const liSidenav = document.querySelectorAll(".sidenav__item");
-    liSidenav.forEach((li) => {
-      li.classList.toggle("active");
-    });
-  } else console.log(e.target);
-});
+    const kiddos = e.target.children;
+    console.log(
+      `Clicked a sidnav button 'component', specifically, a ${e.target.tagName}.`
+    );
 
-const test = document.querySelector("#title-test");
-test.addEventListener("click", () => {
-  test.innerText = "clicked title";
-  console.log(`clicked tilte... did the page preload?`);
-  //   test.preventDefault();
+    // const liSidenav = document.querySelectorAll(".sidenav__item");
+
+    // liSidenav.forEach((li) => {
+    //   //   li.classList.toggle("active");
+    //   li.classList.remove("active");
+    // });
+
+    // if (e.target.tagName.toUpperCase() === "LI") {
+    //   e.target.classList.add("active");
+    // } else console.log(e.target);
+  }
 });
