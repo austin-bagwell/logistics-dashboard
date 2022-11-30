3c1e1bf2e937a639fbc63c398504fef33e8fe0c8
austin-bagwell <austinbagwell.dev@gmail.com>
2022-11-29 21:38:00 -0500

    initial mockup chart creation prep

---
+++ makeChart.js
@@ -0,0 +1,8 @@
+async function makeChart(...args) {
+  // pass in data to make a chart
+  // probably asynchronously, since this will eventually involve a database
+}
+
+// export a chart
+// maybe wrapped in a div?
+// maybe the div wrapping happens elsewhere
