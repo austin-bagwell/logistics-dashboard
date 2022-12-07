b8cf9b5f4d12246f6b93effa254af0ff5c5ab03a
austin-bagwell <austinbagwell.dev@gmail.com>
2022-12-07 13:09:00 -0500

    added v basic csv parsing

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use("/", express.static(path.join(__dirname, "public")));
 
 // FIXME how to connect backend with parcel?
